892f0a8770bf46fef1e78ab197ee8358d0d48311
jtang93 <tang.jonathan93@gmail.com>
2019-03-18 17:12:29 -0400

    completed up to maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+  // let charArray = str.split('')
+  // let distinctChar = new Set(charArray)
+  let countObj = {}
+  // for (char of distinctChar) {
+  //   countObj[char] = 0
+  // }
+
+  for (char of str) {
+    countObj[char]? countObj[char]++ : countObj[char]=1
+    // countObj[char]++
+  }
+
+  return Object.keys(countObj).reduce(
+    (a,b) => (countObj[a]>countObj[b]?a:b)
+  )
+
+  // return countObj
+
+}
 
 module.exports = maxChar;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+
+  //solution 1
+  // return str
+  //   .split('')
+  //   .reverse()
+  //   .join('')
+
+  // solution 2
+  // let reversed = ''
+  //
+  // for (let char of str) {
+  //   reversed = char + reversed
+  // }
+  //
+  // return reversed
+
+  // solution 3
+  let asdf = 1
+  debugger;
+  return str.split('').reduce((reversed, char) => char + reversed, '')
+
+}
+
+reverse('asdf')
 
 module.exports = reverse;
